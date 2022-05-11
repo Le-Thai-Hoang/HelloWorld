d0e8c14399f4f5d7dd4d28a199980fc858de0414
Le-Thai-Hoang <105333656+Le-Thai-Hoang@users.noreply.github.com>
2022-05-11 18:01:13 +0700

    KTra

---
+++ LeThaiHoang1821050063/src/book.java
@@ -0,0 +1,22 @@
+package Lethaihoang1821050063;
+
+import java.util.Scanner;
+
+public class book
+{
+	private String title;
+	private int price,VAT;
+	public void getImortVAT() {
+	VAT=(price*5)/100;
+}
+	void setbook() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap ten sach: ");
+		        title = sc.nextLine();
+		System.out.println("Nhap gia bia: ");
+		        price = sc.nextInt();
+		}  
+		void getbook(){
+		    System.out.println("Ten sach:"+title+"\nGia bia"+price+"\nthue VAT"+VAT);	
+	}
+	}
